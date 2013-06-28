776c44943f8d462d072a681918ba86a835810862
Shusaku NAKAZATO <cu393uc@gmail.com>
2013-06-28 18:33:35 +0900

    compass introduced

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'sass'
+gem 'compass'
 gem 'slim'
 gem 'coffee-script'
 
--- workbench.rb
+++ workbench.rb
@@ -2,9 +2,21 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'slim'
 require 'sass'
+require 'compass'
 require 'coffee-script'
 
 class Workbench < Sinatra::Base
+  configure do
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir = 'views'
+    end
+
+    set :haml, { format: :html5 }
+    set :sass, Compass.sass_engine_options
+    set :scss, Compass.sass_engine_options
+  end
+
   configure :development do
     register Sinatra::Reloader
   end
